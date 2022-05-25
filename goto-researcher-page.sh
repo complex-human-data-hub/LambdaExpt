a6cbd0b435209d1cff8836f6bc9bb001b0b759b9
Ben Stone <benjamin.stone@gmail.com>
2022-05-25 14:36:58 +1000

    Change for Mac OS

--- goto-researcher-page.sh
+++ goto-researcher-page.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 expt_uid=`python config.py`
-xdg-open https://researcher.mall-lab.com/expt?uid=$expt_uid
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux
+    xdg-open https://researcher.mall-lab.com/expt?uid=$expt_uid
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    open https://researcher.mall-lab.com/expt?uid=$expt_uid
+fi
